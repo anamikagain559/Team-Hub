c12f82bb61b1596baba56d202e74615a2d2dbae9
anamikagain559 <anamika.zaimahtech@gmail.com>
2026-05-03 21:00:22 +0600

    Final fix for Gmail SMTP

--- apps/api/src/app/helper/emailHelper.ts
+++ apps/api/src/app/helper/emailHelper.ts
@@ -18,8 +18,9 @@ const sendEmail = async (to: string, subject: string, html: string) => {
   } else {
     config = {
       host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-      port: Number(process.env.EMAIL_PORT) || 587,
-      secure: Number(process.env.EMAIL_PORT) === 465,
+      port: 587,
+      secure: false,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
