f34fb49eda58f752de6a8e2fb52b77b734e24ceb
anamikagain559 <anamika.zaimahtech@gmail.com>
2026-05-03 20:09:05 +0600

    Support secure Google SMTP

--- apps/api/src/app/helper/emailHelper.ts
+++ apps/api/src/app/helper/emailHelper.ts
@@ -6,7 +6,7 @@ const sendEmail = async (to: string, subject: string, html: string) => {
   const config = {
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT),
-    secure: false, // true for 465, false for other ports
+    secure: Number(process.env.EMAIL_PORT) === 465, // true for 465, false for 587
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
